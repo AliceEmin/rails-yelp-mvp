8f9fddfe592eb7abf74fde426f8d3557a8c92568
alice emin <aliceemin@yahoo.com>
2024-07-03 16:42:18 +0200

    premier jet exercice 03-07-2024

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,52 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Restaurant.create!(
+  name: "La Tour d'Argent",
+  address: '11 quai de la Tournelle',
+  phone_number: '06 70 89 65 51',
+  category: 'chinese'
+)
+
+Restaurant.create!(
+  name: "La Tour d'Argent 2",
+  address: '12 quai de la Tournelle',
+  phone_number: '06 70 89 65 52',
+  category: 'chinese'
+)
+
+Restaurant.create!(
+  name: "La Tour d'Argent 3",
+  address: '13 quai de la Tournelle',
+  phone_number: '06 70 89 65 53',
+  category: 'chinese'
+)
+
+Restaurant.create!(
+  name: "La Tour d'Argent 4",
+  address: '14 quai de la Tournelle',
+  phone_number: '06 70 89 65 54',
+  category: 'chinese'
+)
+
+Restaurant.create!(
+  name: "La Tour d'Argent 5",
+  address: '15 quai de la Tournelle',
+  phone_number: '06 70 89 65 55',
+  category: 'chinese'
+)
+
+Restaurant.create!(
+  name: "La Tour d'Argent 6",
+  address: '16 quai de la Tournelle',
+  phone_number: '06 70 89 65 56',
+  category: 'chinese'
+)
+
+Restaurant.create!(
+  name: "La Tour d'Argent 7",
+  address: '17 quai de la Tournelle',
+  phone_number: '06 70 89 65 57',
+  category: 'chinese'
+)
